edf13b14955a131c00abe915b97b02dfd560748e
bob8677 <11bob8677@gmail.com>
2020-08-16 13:31:05 -0500

    modified:   powerline-prompt.sh

--- powerline-prompt.sh
+++ powerline-prompt.sh
@@ -8,6 +8,6 @@ set col2 '\e[43m'
 set fgcol2 '\e[33m'
 set text '\e[39m'
 
-echo -ne $col1(date +%I:%M) $fgcol1$col2 $text$USER $col1$fgcol2$text $hostname'  '
+echo -ne $col1(date +%I:%M) $fgcol1$col2 $text$USER $col1$fgcol2$text $hostname' '
 
 
