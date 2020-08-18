6d68822ce7287d0c9e7b67190ffa76ffbbb083c1
bob8677 <11bob8677@gmail.com>
2020-08-18 13:08:39 -0500

    modified:   summon.sh

--- summon.sh
+++ summon.sh
@@ -1,5 +1,5 @@
 #!/bin/fish
 [ (du ~/.windowlist | awk '{print $1;}') = 0 ] && exit 0
-set WINDOWNAME (cat ~/.windowlist | dmenu -l 10)
+set WINDOWNAME (cat ~/.windowlist | dmenu -l 10 || exit 0) 
 grep -v "$WINDOWNAME" < ~/.windowlist > ~/.windowlisttmp; mv ~/.windowlisttmp ~/.windowlist
 xdotool windowmap (echo $WINDOWNAME | awk 'NF{print $NF}')
